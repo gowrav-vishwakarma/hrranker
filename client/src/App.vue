9a50e89b5b7db0a74cedcdee91430470494648c0
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2021-06-28 22:09:10 +0530

    mode-designer added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <!-- this below v-app was div in starting -->
   <v-app id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </v-app>
 </template>
---
+++ client/src/views/ModelDesigner.vue
@@ -0,0 +1,23 @@
+<template>
+  <model-designer :service="serviceInfo"></model-designer>
+</template>
+
+<script>
+import { ModelDesigner } from "vnatk-vue";
+import api from "../service/api";
+
+export default {
+  name: "ModelDesignerPage",
+  components: {
+    ModelDesigner,
+  },
+  data() {
+    return {
+      serviceInfo: {
+        service: api,
+        basepath: "/admin/vnatk",
+      },
+    };
+  },
+};
+</script>
